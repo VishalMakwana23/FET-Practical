0afefa15a7e0f3241fc5f7ec42a6c2deee67b18b
Vishal Makwana <70998696+VishalMakwana23@users.noreply.github.com>
2021-08-31 15:10:46 +0530

    Add files via upload

---
+++ AngularJson/style.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp',[]);
+
+app.controller('myController',function($scope,$http){
+
+  $http.get('data.json').then(function(response){
+
+    $scope.items = response.data;
+  });
+});
